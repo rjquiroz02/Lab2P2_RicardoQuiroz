343c0bdc540252c21c91e1ddc86f40c15c7e60d3
rjquiroz02 <rjquiroz02@unitec.edu>
2022-07-29 15:38:50 -0600

    Mensaje#6

--- src/lab2p2_ricardoquiroz/Lab2P2_RicardoQuiroz.java
+++ src/lab2p2_ricardoquiroz/Lab2P2_RicardoQuiroz.java
@@ -18,17 +18,46 @@ static Scanner lea = new Scanner(System.in);
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ArrayList lista = new ArrayList();
-        boolean cent;
-        int menu = 0;
         System.out.println("Cuenta administrador: ");
         String admin = lea.next();
-        System.out.println("Contrasena administrador: ");
+        System.out.println("Clave administrador: ");
         String pass = lea.next();
         if (admin.equals("admin") && pass.equals("admin1234")) {            
-            
+            ArrayList lista = new ArrayList();
+            boolean cent;
+            int menu = 0;
+            while (menu != 4) {                
+                cent = false;
+                System.out.println("----------Menu Principal----------");
+                System.out.println("1. Clientes");
+                System.out.println("2. Empleados");
+                System.out.println("3. Carros");
+                System.out.println("Ingrese submenu que desea abrir: ");
+                menu = lea.nextInt();
+                System.out.println("----------------------------------");
+                if (menu == 1) {
+                    System.out.println("----------Clientes----------");
+                    System.out.println("");
+                    System.out.println("----------------------------");
+                    cent = true;
+                }
+                if (menu == 2) {
+                    
+                    cent = true;
+                }
+                if (menu == 3) {
+                    
+                    cent = true;
+                }
+            }
         }
-        
+        while (!admin.equals("admin") || !pass.equals("admin1234")) {  
+            System.out.println("Ingrese la cuenta y clave correcta de la cuenta");
+            System.out.println("Cuenta administrador: ");
+            admin = lea.next();
+            System.out.println("Clave administrador: ");
+            pass = lea.next();
+        }        
         
     }//void main
     
