6716f26ef433011cad741818f417064206a45bdc
rjquiroz02 <rjquiroz02@unitec.edu>
2022-07-29 18:25:03 -0600

    Mensaje#12

--- src/lab2p2_ricardoquiroz/Lab2P2_RicardoQuiroz.java
+++ src/lab2p2_ricardoquiroz/Lab2P2_RicardoQuiroz.java
@@ -127,7 +127,7 @@ static Scanner lea = new Scanner(System.in);
                         if (submenu2 == 1) {
                             String nom;
                             int e, s, r;
-                            String cond;
+                            //String cond;
                             boolean t;
                             System.out.print("Nombre: ");
                             lea.nextLine();
@@ -138,36 +138,75 @@ static Scanner lea = new Scanner(System.in);
                             s = lea.nextInt();
                             System.out.print("Numero RRHH: ");
                             r = lea.nextInt();
-                            System.out.println("Esta trabajando[s/n]: ");
-                            cond = lea.next();
-                            if (cond.contains("s")) {
-                                t = true;
-                            }
-                            else{
-                                t = false;
-                            }
+                            t = false;
                             lista.add(new Empleado(nom, e, s, r, t));
                             
                             cent2 = true;
                         }
                         if (submenu2 == 2) {
                             int indice;
-                            
+                            boolean t;
+                            String cond;
+                            System.out.println("Ingrese el indice del empleado a modificar: ");
+                            indice = lea.nextInt();
+                            if (indice <= lista.size()-1 && lista.get(indice) instanceof Empleado) {
+                                System.out.println("Esta trabajando[s/n]: ");
+                                cond = lea.next();
+                                if (cond.contains("s")) {
+                                    t = true;
+                                }
+                                else{
+                                    t = false;
+                                }
+                                ((Empleado)lista.get(indice)).setTrabaja(t);
+                                System.out.println("Empleado modificado");
+                            }
                             
                             cent2 = true;
                         }
                         if (submenu2 == 3) {
-                            
+                            int indice;
+                            System.out.print("Ingrese indice del empleado a borrar: ");
+                            indice = lea.nextInt();
+                            if (indice <= lista.size()-1 && lista.get(indice) instanceof Empleado) {
+                                lista.remove(indice);
+                                System.out.println("Empleado eliminado");
+                            }
+                            else{
+                                System.out.println("No se cumplen los requisitos");
+                            }
                             
                             cent2 = true;
                         }
                         if (submenu2 == 4) {
-                            
+                            String out = "";
+                            for (Object object : lista) {
+                                if (object instanceof Empleado) {
+                                    out+=lista.indexOf(object)+"-> "+((Empleado) object).toStringEm()+"\n";
+                                }
+                            }
+                            System.out.println(out);
                             
                             cent2 = true;
                         }
                         if (submenu2 == 5) {
-                            
+                            String cond;
+                            boolean t;                           
+                            int indice;
+                            System.out.println("Ingrese el indice del empleado a modificar: ");
+                            indice = lea.nextInt();
+                            if (indice <= lista.size()-1 && lista.get(indice) instanceof Empleado) {
+                                System.out.println("Esta trabajando[s/n]: ");
+                                cond = lea.next();
+                                if (cond.contains("s")) {
+                                    t = true;
+                                }
+                                else{
+                                    t = false;
+                                }
+                                ((Empleado)lista.get(indice)).setTrabaja(t);
+                                System.out.println("Empleado modificado");
+                            }
                             
                             cent2 = true;
                         }
@@ -180,24 +219,52 @@ static Scanner lea = new Scanner(System.in);
                 if (menu == 3) {
                     int submenu3 = 0;
                     boolean cent3;
-                    while (submenu3 != 5) {  
+                    while (submenu3 != 4) {  
                         cent3 = false;
                         System.out.println("---------Carros----------");
                         System.out.println("1. Crear");
                         System.out.println("2. Modificar");
-                        System.out.println("3. Eliminar");
-                        System.out.println("4. Listar por su estado");
-                        System.out.println("5. Salir");
+                        System.out.println("3. Listar por su estado");
+                        System.out.println("4. Salir");
                         System.out.println("Ingrese opcion del submenu: ");
                         submenu3 = lea.nextInt();
                         System.out.println("-------------------------");
                         if (submenu3 == 1) {
-                            
+                            String marca, modelo, fechaingreso, estado, ref;
+                            int anofab, costorep;
+                            System.out.print("Marca: ");
+                            lea.nextLine();
+                            marca = lea.nextLine();
+                            System.out.print("Modelo: ");
+                            lea.nextLine();
+                            modelo = lea.nextLine();
+                            System.out.print("Ano Fabricado: ");
+                            anofab = lea.nextInt();
+                            System.out.println("Fecha ingreso: ");
+                            fechaingreso = lea.next();
+                            System.out.println("Estado: ");
+                            lea.nextLine();
+                            estado = lea.nextLine();
+                            System.out.println("Referencia Dueno: ");
+                            lea.nextLine();
+                            ref = lea.nextLine();
+                            System.out.println("Costo Reparacion: ");
+                            costorep = lea.nextInt();
+                            lista.add(new Carro(marca, modelo, anofab, fechaingreso, estado, ref, costorep));
                             
                             cent3 = true;
                         }
                         if (submenu3 == 2) {
-                            
+                            int indice;
+                            String estado="";
+                            System.out.println("Ingrese el indice del carro a modificar: ");
+                            indice = lea.nextInt();
+                            if (indice <= lista.size()-1 && lista.get(indice) instanceof Carro) {
+                                System.out.println("Ingrese estado: ");
+                                estado = lea.nextLine();
+                                ((Carro)lista.get(indice)).setEstado(estado);
+                                System.out.println("Carro modificado");
+                            }
                             
                             cent3 = true;
                         }
@@ -206,12 +273,7 @@ static Scanner lea = new Scanner(System.in);
                             
                             cent3 = true;
                         }
-                        if (submenu3 == 4) {
-                            
-                            
-                            cent3 = true;
-                        }
-                        if (!cent3 && submenu3 != 5) {
+                        if (!cent3 && submenu3 != 4) {
                             System.out.println("Ingrese opcion correcta del submenu");
                         }
                     }
